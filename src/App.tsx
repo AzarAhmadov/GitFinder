b7915c25b9102e5056dd48212b963fcafb2763b0
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-10 13:21:05 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { FC } from 'react'
+import Layout from './layout/Layout'
 
-const App: FC = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App: FC = () => <Layout />
+
 
 export default App
