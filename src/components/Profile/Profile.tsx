49831ac67366093c300b34826db9f92e06b99952
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-10 19:48:20 +0400

    Update Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
 import { FiGithub } from "react-icons/fi";
+import { BiGitRepoForked } from "react-icons/bi";
 import { SlLocationPin } from "react-icons/sl";
 import './Profile.css'
 
@@ -9,6 +10,7 @@ const Profile: FC = () => {
         <section id='profile'>
             <img className='user-img' src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D" alt="" />
             <div className="info">
+
                 <span className="name">
                     Azar Ahmadov
                 </span>
@@ -18,14 +20,24 @@ const Profile: FC = () => {
                 <p className='desc'>
                     3nd Year Computer Science Student Junior Front-end Developer
                 </p>
-                <Link className='flex flex-center' to={'/'}>
+                <Link className='github-link flex flex-center' to={'/'}>
                     <FiGithub />
                     See on Github
                 </Link>
-                <span className="location flex flex-align-center">
-                    <SlLocationPin />  Azerbaijan , Baku
-                </span>
-                <ul className='flex gap-10'>
+
+                <ul className='user-links'>
+                    <li className='flex flex-align-center'>
+                        <SlLocationPin />  Azerbaijan,Baku
+                    </li>
+                    <li className='flex flex-align-center'>
+                        <BiGitRepoForked />
+                        <Link to={''}>
+                            link
+                        </Link>
+                    </li>
+                </ul>
+
+                <ul className='user-action flex gap-10'>
                     <li className='flex'>
                         <span>72</span>
                         Repos
