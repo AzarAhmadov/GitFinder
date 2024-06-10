a6924348a79c5b4fd732e568baedbad98c947f71
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-10 20:55:36 +0400

    Update Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,57 +1,92 @@
 import { FC } from 'react'
+import './Profile.css'
 import { Link } from 'react-router-dom'
 import { FiGithub } from "react-icons/fi";
-import { BiGitRepoForked } from "react-icons/bi";
+import { RiBuilding2Line } from "react-icons/ri";
 import { SlLocationPin } from "react-icons/sl";
-import './Profile.css'
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { useQuery } from 'react-query';
+import { GetGithubUser } from '../../services/Api';
+import { kFormatter } from '../../helper/numberFormat';
+import { User } from '../../types/type';
 
 const Profile: FC = () => {
+
+    const { isLoading, data } = useQuery<User>('githubUser', () => GetGithubUser('azarahmadov'));
+
     return (
         <section id='profile'>
-            <img className='user-img' src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D" alt="" />
-            <div className="info">
+            <img
+                className='user-img'
+                src={data?.avatar_url}
+                alt={data?.name}
+            />
 
-                <span className="name">
-                    Azar Ahmadov
-                </span>
-                <span className="nick-name">
-                    azarahmadov
-                </span>
-                <p className='desc'>
-                    3nd Year Computer Science Student Junior Front-end Developer
-                </p>
-                <Link className='github-link flex flex-center' to={'/'}>
-                    <FiGithub />
-                    See on Github
-                </Link>
-
-                <ul className='user-links'>
-                    <li className='flex flex-align-center'>
-                        <SlLocationPin />  Azerbaijan,Baku
-                    </li>
-                    <li className='flex flex-align-center'>
-                        <BiGitRepoForked />
-                        <Link to={''}>
-                            link
+            {
+                isLoading ? <p>loading...</p> :
+                    <div className="info">
+                        <span className="name">
+                            {data?.name}
+                        </span>
+                        <span className="nick-name">
+                            {data?.login}
+                        </span>
+                        <p className='desc'>
+                            {data?.bio}
+                        </p>
+                        <Link target='_blank' className='github-link flex flex-center' to={`${data?.html_url}`}>
+                            <FiGithub />
+                            See on Github
                         </Link>
-                    </li>
-                </ul>
 
-                <ul className='user-action flex gap-10'>
-                    <li className='flex'>
-                        <span>72</span>
-                        Repos
-                    </li>
-                    <li className='flex'>
-                        <span>12</span>
-                        Followers
-                    </li>
-                    <li className='flex'>
-                        <span>39</span>
-                        Following
-                    </li>
-                </ul>
-            </div>
+                        <ul className='user-links'>
+                            {
+                                data?.location &&
+                                <li className='flex flex-align-center'>
+                                    <SlLocationPin />  {data.location}
+                                </li>
+                            }
+                            {
+                                data?.company &&
+                                <li className='flex flex-align-center'>
+                                    <RiBuilding2Line />  {data.company}
+                                </li>
+                            }
+                            {
+                                data?.blog &&
+                                <li className='flex flex-align-center'>
+                                    <FaExternalLinkAlt /> <Link target='_blank' to={`${data.blog}`}> {data.blog}</Link>
+                                </li>
+                            }
+                        </ul>
+
+                        <ul className='user-action flex gap-10'>
+                            {
+                                data?.public_repos
+                                &&
+                                <li className='flex'>
+                                    <span>{data?.public_repos}</span>
+                                    Repos
+                                </li>
+                            }
+                            {
+                                data?.followers &&
+                                <li className='flex'>
+                                    <span>{kFormatter(data?.followers)}</span>
+                                    Following
+                                </li>
+                            }
+
+                            {
+                                data?.following &&
+                                <li className='flex'>
+                                    <span>{data?.following}</span>
+                                    Following
+                                </li>
+                            }
+                        </ul>
+                    </div>
+            }
         </section>
     )
 }
