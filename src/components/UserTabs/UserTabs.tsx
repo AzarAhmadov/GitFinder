bf111fbac3cd96ba299034a9775570bc7c9cdc79
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-10 19:48:32 +0400

    Update UserTabs.tsx

--- src/components/UserTabs/UserTabs.tsx
+++ src/components/UserTabs/UserTabs.tsx
@@ -1,31 +1,57 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import './UserTabs.css'
+import FollowCards from '../Cards/FollowCards/FollowCards'
+import RepoCards from '../Cards/RepoCards/RepoCards';
 
 const UserTabs: FC = () => {
+
+    const [activeTab, setActiveTab] = useState(0);
+
+    const handleTabClick = (index: number) => {
+        setActiveTab(index);
+    };
+
     return (
         <section id='tabs'>
             <ul className='tabs-header flex flex-between'>
-                <li className='active'>
+                <li onClick={() => handleTabClick(0)} className={activeTab === 0 ? 'active' : ''}>
                     <button>
                         Repositories
                     </button>
                 </li>
-                <li>
+                <li onClick={() => handleTabClick(1)} className={activeTab === 1 ? 'active' : ''}>
                     <button>
                         Forked
                     </button>
                 </li>
-                <li>
+                <li onClick={() => handleTabClick(2)} className={activeTab === 2 ? 'active' : ''}>
                     <button>
                         Followers
                     </button>
                 </li>
-                <li>
+                <li onClick={() => handleTabClick(3)} className={activeTab === 3 ? 'active' : ''}>
                     <button>
                         Following
                     </button>
                 </li>
             </ul>
+
+            {
+                activeTab === 0 && <ul className='tabs-item-list'>
+                    <li>
+                        <RepoCards />
+                    </li>
+                </ul>
+            }
+
+
+            {
+                activeTab === 1 && <ul className='tabs-item-list'>
+                    <li>
+                        <FollowCards />
+                    </li>
+                </ul>
+            }
         </section>
     )
 }
