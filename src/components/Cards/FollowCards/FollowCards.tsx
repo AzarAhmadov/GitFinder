4ee044d5091f612e15deb17de08491ecbb2307e7
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-10 19:48:04 +0400

    Update FollowCards.tsx

--- src/components/Cards/FollowCards/FollowCards.tsx
+++ src/components/Cards/FollowCards/FollowCards.tsx
@@ -1,9 +1,21 @@
 import { FC } from 'react'
 import './FollowCards.css'
+import { Link } from 'react-router-dom'
+import { IoLinkSharp } from "react-icons/io5";
 
 const FollowCards: FC = () => {
     return (
-        <section>FollowCards</section>
+        <div className='follow-card flex flex-align-center flex-between'>
+            <div className='flex flex-align-center'>
+                <img src="https://avatars.githubusercontent.com/u/39780?v=4" alt="" />
+                <span>
+                    user name
+                </span>
+            </div>
+            <Link className='flex flex-center' to={''}>
+                <IoLinkSharp />
+            </Link>
+        </div>
     )
 }
 
