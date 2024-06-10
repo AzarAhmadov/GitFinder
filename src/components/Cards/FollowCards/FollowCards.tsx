932968f10a2b2ec9e440ea432fd373e64dfb0b10
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-10 13:24:55 +0400

    Updated

--- src/components/Cards/FollowCards/FollowCards.tsx
+++ src/components/Cards/FollowCards/FollowCards.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import './FollowCards.css'
 
 const FollowCards: FC = () => {
     return (
--- src/components/Cards/RepoCards/RepoCards.tsx
+++ src/components/Cards/RepoCards/RepoCards.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import './RepoCards.css'
 
 const RepoCards: FC = () => {
     return (
