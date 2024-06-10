65f1c166cb1317cb98eee7c9b35368017a2031a6
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-10 20:56:05 +0400

    add User type

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,3 +1,17 @@
 export type ContainerType = {
     children: React.ReactNode
 }
+
+export type User = {
+    avatar_url: string;
+    name: string;
+    login: string;
+    bio: string;
+    html_url: string;
+    location?: string;
+    company?: string;
+    blog?: string;
+    public_repos: number;
+    followers: number;
+    following: number;
+};
