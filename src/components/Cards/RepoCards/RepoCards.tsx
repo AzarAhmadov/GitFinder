ad9f82e749e858a3405bd896653738ea1538ad14
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-11 11:48:48 +0400

    Update RepoCards.tsx

--- src/components/Cards/RepoCards/RepoCards.tsx
+++ src/components/Cards/RepoCards/RepoCards.tsx
@@ -4,35 +4,46 @@ import { RxStarFilled } from "react-icons/rx";
 import { BiGitRepoForked } from "react-icons/bi";
 import './RepoCards.css'
 import { Link } from 'react-router-dom';
+import { RepoType } from '../../../types/type';
 
-const RepoCards: FC = () => {
+interface IRepo {
+    el: RepoType
+}
+
+const RepoCards: FC<IRepo> = ({ el }) => {
+
+    const { name, visibility, language, description, forks_count, stargazers_count
+    } = el
+
     return (
         <div className='repo-cards'>
             <div className="cards-top">
                 <div className='link flex flex-between flex-align-center'>
                     <Link to={''}>
-                        Repo name
+                        {name}
                     </Link>
                     <span className="status">
-                        Public
+                        {visibility}
                     </span>
                 </div>
-                <p className='desc-repo'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nihil aspernatur molestiae necessitatibus ducimus saepe ex explicabo, a odio iste!
-                </p>
+                <div className='desc-area'>
+                    <p className='desc-repo'>
+                        {description}
+                    </p>
+                </div>
             </div>
             <ul className='flex gap-15'>
                 <li className='flex'>
                     <IoCodeSlash />
-                    Javascript
+                    {language}
                 </li>
                 <li>
                     <RxStarFilled />
-                    10
+                    {stargazers_count}
                 </li>
                 <li>
                     <BiGitRepoForked />
-                    10
+                    {forks_count}
                 </li>
             </ul>
         </div>
