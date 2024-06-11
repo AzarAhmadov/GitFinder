c18b2bc9ee4c9dda45ebbf7dc7c481ed95edc47a
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-11 11:47:44 +0400

    add new fetching end point

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,10 +1,10 @@
 // Fetch User
 
-export const GetGithubUser = async (user: string) => {
+export const GithubUser = async (user: string) => {
     try {
         const res = await fetch(`https://api.github.com/users/${user}`)
         if (!res.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Failed to fetch GitHub user data');
         }
         const data = await res.json()
         return data
@@ -12,4 +12,34 @@ export const GetGithubUser = async (user: string) => {
     } catch (err) {
         console.log(err)
     }
+}
+
+export const GithubFollowers = async (user: string) => {
+    try {
+
+        const res = await fetch(`https://api.github.com/users/${user}/followers`)
+        if (!res.ok) {
+            throw new Error('Failed to fetch GitHub followers data');
+        }
+        const data = await res.json()
+        return data
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export const GithubRepos = async (user: string) => {
+
+    try {
+        const res = await fetch(`https://api.github.com/users/${user}/repos`)
+        if (!res.ok) {
+            throw new Error('Failed to fetch GitHub repos data');
+        }
+        const data = await res.json()
+        return data
+    } catch (err) {
+        console.log(err)
+    }
+
 }
