8859a44c9bb5f28212115fca104de36a709e9263
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-11 11:46:53 +0400

    Update type.ts

--- src/types/type.ts
+++ src/types/type.ts
@@ -4,14 +4,23 @@ export type ContainerType = {
 
 export type User = {
     avatar_url: string;
-    name: string;
-    login: string;
-    bio: string;
-    html_url: string;
+    name?: string;
+    login?: string;
+    bio?: string;
+    html_url?: string;
     location?: string;
     company?: string;
     blog?: string;
-    public_repos: number;
-    followers: number;
-    following: number;
+    public_repos?: number;
+    followers?: number;
+    following?: number;
 };
+
+export type RepoType = {
+    name: string,
+    visibility: boolean,
+    language: string,
+    description: string,
+    forks_count: number,
+    stargazers_count: number
+}
