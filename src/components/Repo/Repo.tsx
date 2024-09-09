b8ea17cd3eaad9d20a363a3240463e82cb9d6b47
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-09 10:17:30 +0400

    Update Repo.tsx

--- src/components/Repo/Repo.tsx
+++ src/components/Repo/Repo.tsx
@@ -1,44 +1,43 @@
-import { FC, useContext } from 'react'
-import { useQuery } from 'react-query';
-import { GithubRepos } from '../../services/Api';
-import RepoCards from '../Cards/RepoCards/RepoCards';
-import { RepoType } from '../../types/type';
-import { SearchContext } from '../../services/context/context';
-import Loading from '../Loading/Loading';
-import ErrorMsg from '../Error/ErrorMsg';
+import { FC, useContext } from "react";
+import { useQuery } from "react-query";
+import { GithubRepos } from "../../services/Api";
+import RepoCards from "../Cards/RepoCards/RepoCards";
+import { RepoType } from "../../types/type";
+import { SearchContext } from "../../services/context/context";
+import Loading from "../Loading/Loading";
+import ErrorMsg from "../Error/ErrorMsg";
 
 const Repo: FC = () => {
+  const { search } = useContext<any>(SearchContext);
 
-    const { search } = useContext<any>(SearchContext);
+  const { isLoading: reposLoading, data: Repos } = useQuery(
+    ["githubRepos", search],
+    () => GithubRepos(search),
+    {
+      refetchOnWindowFocus: true,
+      staleTime: 0,
+      cacheTime: 0,
+      refetchInterval: 0,
+    }
+  );
 
-    const { isLoading: reposLoading, data: Repos } = useQuery(
-        ['githubRepos', search],
-        () => GithubRepos(search),
-        {
-            refetchOnWindowFocus: true,
-            staleTime: 0,
-            cacheTime: 0,
-            refetchInterval: 0,
-        }
-    );
+  if (Repos && Repos.length === 0) return <ErrorMsg err=" repositories" />;
 
-    if (Repos && Repos.length === 0) return <ErrorMsg err='repositories' />
+  return (
+    <>
+      {reposLoading ? (
+        <Loading />
+      ) : (
+        <ul className="tabs-item-list">
+          {Repos?.map((el: RepoType, idx: number) => (
+            <li key={idx}>
+              <RepoCards el={el} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
 
-    return (
-        <>
-            {reposLoading ? (
-                <Loading />
-            ) : (
-                <ul className='tabs-item-list'>
-                    {Repos?.map((el: RepoType, idx: number) => (
-                        <li key={idx}>
-                            <RepoCards el={el} />
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </>
-    )
-}
-
-export default Repo
+export default Repo;
