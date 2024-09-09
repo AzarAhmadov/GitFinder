d9b4e76218affa6bd0b9f0c00cd231c6917b28db
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-09 10:17:26 +0400

    Update Layout.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,43 +1,34 @@
-import { useContext } from 'react'
-import Header from '../common/Header/Header'
-import { Outlet } from 'react-router-dom'
-import { useQuery } from 'react-query';
-import { GithubUser } from '../services/Api';
-import { User } from '../types/type';
-import { SearchContext } from '../services/context/context';
-import Page404 from '../components/Page404/Page404';
+import { useContext } from "react";
+import { Outlet } from "react-router-dom";
+import { useQuery } from "react-query";
+import { GithubUser } from "../services/Api";
+import { User } from "../types/type";
+import { SearchContext } from "../services/context/context";
+import Page404 from "../components/Page404/Page404";
 
 const Layout = () => {
+  const { search } = useContext<any>(SearchContext);
 
-    const { search } = useContext<any>(SearchContext);
+  const { isError, data: githubUser } = useQuery<User>(
+    ["githubUser", search],
+    () => GithubUser(search),
+    {
+      refetchOnWindowFocus: true,
+      staleTime: 0,
+      cacheTime: 0,
+      refetchInterval: 0,
+    }
+  );
 
-    const { data: githubUser } = useQuery<User>(
-        ['githubUser', search],
-        () => GithubUser(search),
-        {
-            refetchOnWindowFocus: true,
-            staleTime: 0,
-            cacheTime: 0,
-            refetchInterval: 0,
-        }
-    );
+  if (isError || !githubUser) return <Page404 />;
 
-    if (githubUser === undefined)
-        return (
-            <>
-                <Header />
-                <Page404 />
-            </>
-        )
+  return (
+    <>
+      <main id="main">
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
-    return (
-        <>
-            <Header />
-            <main id='main'>
-                <Outlet />
-            </main>
-        </>
-    )
-}
-
-export default Layout
+export default Layout;
