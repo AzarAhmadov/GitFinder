855fa9db9e2c8dbf966ee7514c49dee9e5b4872a
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-09 10:17:24 +0400

    Update routes.tsx

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,19 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import Page404 from "../components/Page404/Page404";
 import HomeScreen from "../pages/HomeScreen/HomeScreen";
-import App from "../App";
+import Header from "../common/Header/Header";
+import Layout from "../layout/Layout";
 
 const router = createBrowserRouter([
-    {
-        element: <App />,
-        errorElement: <Page404 />,
-        children: [
-            {
-                path: "/",
-                element: <HomeScreen />,
-            },
-        ],
-    },
-])
+  {
+    element: (
+      <>
+        <Header />
+        <Layout />
+      </>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <HomeScreen />,
+      },
+    ],
+    errorElement: <Page404 />,
+  },
+]);
 
-export default router
+export default router;
